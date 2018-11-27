48cfdec1115acd264d1f1c14e1e897398afc3096
Osman Bineev <bineev@gmail.com>
2018-11-27 22:00:03 +0300

    tes/ts added

---
+++ test/ts/index.tsx
@@ -0,0 +1,19 @@
+import {
+    ILinkAttributes,
+    Link,
+    IRedirectAttributes,
+    Redirect,
+    IRouteAttributes,
+    ITargetAttributes,
+    Route,
+    TargetComponent,
+    TargetNode,
+    Child,
+    IRendererOwner,
+    JSXFactory,
+    Router,
+    Switch } from "router";
+
+import * as ui from "hyperoop";
+
+// TODO
